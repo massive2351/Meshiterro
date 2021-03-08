a34a49bbc8ac73e041fc72b1a5ff2f9955c79a9e
massive2351 <massive2351@gmail.com>
2021-03-08 03:50:23 +0000

    finish 3-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'dd4e43d6658bc02b5625cd702ed7c1f5774a2af85491bfd99c95686a53ea1c5c0fabd36f92f04b73fda5bbb205243379f833fd463693b8a457fe14260faea1f4'
